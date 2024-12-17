c6631f144d620d6e17a3439f5e0410a390ac3154
Trikolon <git@emz.im>
2024-12-17 23:35:35 +0100

    Include 404 page in vite build

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        404: '404.html'
+      }
+    }
+  }
+}) 
